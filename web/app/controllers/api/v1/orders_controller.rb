01daf8e15a10e93b2c054231e2527dd9d6b9e94b
David Weirich <david@dweirich.com>
2022-09-13 18:26:34 -0700

    Order orders descending

--- web/app/controllers/api/v1/orders_controller.rb
+++ web/app/controllers/api/v1/orders_controller.rb
@@ -8,11 +8,11 @@ module Api
 
         case pagination_params[:query]
         when 'overdue'
-          orders = current_user.orders.payment_due
+          orders = current_user.orders.payment_due.order(shopify_created_at: :desc)
         when 'pending'
-          orders = current_user.orders.pending
+          orders = current_user.orders.pending.order(shopify_created_at: :desc)
         else
-          orders = current_user.orders
+          orders = current_user.orders.order(shopify_created_at: :desc)
         end
 
         render json: orders
