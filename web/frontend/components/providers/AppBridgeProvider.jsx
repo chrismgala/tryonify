0fe285ef920ae9c1709c3d6ce71066e9b5aeb717
David Weirich <david@dweirich.com>
2022-10-04 22:10:39 -0700

    Add AppProvider

--- web/frontend/components/providers/AppBridgeProvider.jsx
+++ web/frontend/components/providers/AppBridgeProvider.jsx
@@ -1,7 +1,9 @@
 import { useMemo, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Provider } from "@shopify/app-bridge-react";
-import { Banner, Layout, Page } from "@shopify/polaris";
+import { AppProvider, Banner, Layout, Page } from "@shopify/polaris";
+import translations from '@shopify/polaris/locales/en.json';
+import Link from "../link";
 
 /**
  * A component to configure App Bridge.
@@ -45,31 +47,32 @@ export function AppBridgeProvider({ children }) {
       host,
       apiKey: process.env.SHOPIFY_API_KEY,
       forceRedirect: true,
+
     };
   });
 
   if (!process.env.SHOPIFY_API_KEY || !appBridgeConfig.host) {
     const bannerProps = !process.env.SHOPIFY_API_KEY
       ? {
-          title: "Missing Shopify API Key",
-          children: (
-            <>
-              Your app is running without the SHOPIFY_API_KEY environment
-              variable. Please ensure that it is set when running or building
-              your React app.
-            </>
-          ),
-        }
+        title: "Missing Shopify API Key",
+        children: (
+          <>
+            Your app is running without the SHOPIFY_API_KEY environment
+            variable. Please ensure that it is set when running or building
+            your React app.
+          </>
+        ),
+      }
       : {
-          title: "Missing host query argument",
-          children: (
-            <>
-              Your app can only load if the URL has a <b>host</b> argument.
-              Please ensure that it is set, or access your app using the
-              Partners Dashboard <b>Test your app</b> feature
-            </>
-          ),
-        };
+        title: "Missing host query argument",
+        children: (
+          <>
+            Your app can only load if the URL has a <b>host</b> argument.
+            Please ensure that it is set, or access your app using the
+            Partners Dashboard <b>Test your app</b> feature
+          </>
+        ),
+      };
 
     return (
       <Page narrowWidth>
@@ -86,7 +89,9 @@ export function AppBridgeProvider({ children }) {
 
   return (
     <Provider config={appBridgeConfig} router={routerConfig}>
-      {children}
+      <AppProvider i18n={translations} linkComponent={Link}>
+        {children}
+      </AppProvider>
     </Provider>
   );
 }
