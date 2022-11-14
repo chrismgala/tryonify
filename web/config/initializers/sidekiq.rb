1b95b108613886bccbb67f583dc709405b53f2f9
David Weirich <david@dweirich.com>
2022-11-14 13:33:55 -0800

    Update Sidekiq network timeout

--- web/config/initializers/sidekiq.rb
+++ web/config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1').presence }
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1').presence, network_timeout: 5 }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1').presence }
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1').presence, network_timeout: 5 }
 end
