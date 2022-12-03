01930fe870ed815eaa1fea4edaae6c1b3d5adad2
David Weirich <david@dweirich.com>
2022-12-02 18:48:47 -0800

    Extend trial by 60 days rake task

---
+++ web/app/services/extend_trial.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+class ExtendTrial
+  class InvalidRequest < StandardError; end
+
+  EXTEND_TRIAL_QUERY = <<~QUERY
+    mutation appSubscriptionTrialExtend($days: Int!, $id: ID!) {
+      appSubscriptionTrialExtend(days: $days, id: $id) {
+        appSubscription {
+          id
+          trialDays
+        }
+        userErrors {
+          field
+          message
+        }
+      }
+    }
+  QUERY
+
+  def initialize
+    session = ShopifyAPI::Context.active_session
+    @client = ShopifyAPI::Clients::Graphql::Admin.new(session:)
+  end
+
+  def call
+    service = FetchAppSubscription.new
+    service.call
+
+    raise "No subscription found" unless service.app
+
+    variables = {
+      days: 60,
+      id: service.app.dig("activeSubscriptions", 0, "id"),
+    }
+
+    response = @client.query(query: EXTEND_TRIAL_QUERY, variables:)
+
+    unless response.body["errors"].nil?
+      raise ExtendTrial::InvalidRequest,
+        response.body.dig("errors", 0, "message") and return
+    end
+
+    @order = response.body.dig("data", "order")
+  end
+end
