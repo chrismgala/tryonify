cea154ad2b4b1cdc58412203eacdf5ca95eff00d
David Weirich <david@dweirich.com>
2023-09-18 11:38:04 -0700

    Rollback migration

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,24 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_12_222339) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_10_194208) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
-
-  create_table "bulk_operations", force: :cascade do |t|
-    t.string "shopify_id"
-    t.datetime "completed_at"
-    t.string "error_code"
-    t.string "url"
-    t.string "status"
-    t.text "query"
-    t.bigint "shop_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["shop_id"], name: "index_bulk_operations_on_shop_id"
-    t.index ["shopify_id"], name: "index_bulk_operations_on_shopify_id", unique: true
-  end
 
   create_table "checkouts", force: :cascade do |t|
     t.string "draft_order_id", null: false
@@ -265,7 +251,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_12_222339) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "bulk_operations", "shops"
   add_foreign_key "checkouts", "shops"
   add_foreign_key "line_items", "orders"
   add_foreign_key "orders", "shops", on_delete: :cascade
