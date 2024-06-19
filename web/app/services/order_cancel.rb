d381e64714cd12a1a4ef0e4b90dc354906b3b507
David Weirich <david@dweirich.com>
2024-06-19 12:38:04 -0700

    Use GraphQL API for order cancel

--- web/app/services/order_cancel.rb
+++ web/app/services/order_cancel.rb
@@ -21,29 +21,13 @@ class OrderCancel < ApplicationService
   private
 
   def cancel_order
-    suggested_refund = OrderSuggestedRefund.call(@order) if @refund
-
-    shopify_order = ShopifyAPI::Order.find(id: @order.shopify_id.split("/").pop, session: @session)
-    refund = {
-      note: "Cancelled by TryOnify",
-      refund_line_items: suggested_refund&.body&.dig("data", "order", "suggestedRefund", "refundLineItems")&.map do |refund_line_item|
-        {
-          line_item_id: refund_line_item.dig("lineItem", "id")&.split("/")&.pop,
-          quantity: refund_line_item["quantity"],
-          restock_type: "cancel",
-          location_id: refund_line_item.dig("location", "id")&.split("/")&.pop,
-        }
-      end,
-      transactions: suggested_refund&.body&.dig("data", "order", "suggestedRefund", "suggestedTransactions")&.map do |suggested_transaction|
-        {
-          parent_id: suggested_transaction.dig("parentTransaction", "id")&.split("/")&.pop,
-          amount: suggested_transaction.dig("amountSet", "shopMoney", "amount"),
-          kind: "refund",
-          gateway: suggested_transaction["gateway"],
-        }
-      end
-    }
-    shopify_order.cancel(refund: refund || nil, email: true, session: @session)
+    Shopify::Orders::Cancel.call(
+      order_id: @order.shopify_id,
+      refund: @refund,
+      restock: true,
+      reason: "DECLINED",
+      staff_note: "Cancelled by TryOnify"
+    )
   end
 
   def send_notifications
