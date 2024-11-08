18cd1af1b6c20e62515afcfb392898f9e32eedaf
Chris Gala <cg@chargeholdings.co>
2024-11-07 23:32:05 -0800

    Update metafield controller to pull in selling plan group id

--- web/app/controllers/metafield_controller.rb
+++ web/app/controllers/metafield_controller.rb
@@ -5,12 +5,20 @@ class MetafieldController < ApplicationController
     gid_string = "gid://shopify/Metafield/#{params[:shopify_id]}"
     metafield = Metafield.find_by(shopify_id: gid_string)
 
+    selling_plan_group = SellingPlanGroup.find_by(shop_id: metafield.shop_id);
+
     render(json: { message: "Metafield not found" }) && return unless metafield
+    render(json: { message: "Selling plan group not found" }) && return unless selling_plan_group
 
     render(json: {
       data: {
-        key: metafield.key,
-        value: metafield.value,
+        metafield: {
+          key: metafield.key,
+          value: metafield.value,
+        },
+        selling_plan_group: {
+          shopify_id: selling_plan_group.shopify_id,
+        }
       }
     })
   end
