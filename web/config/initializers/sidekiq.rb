0019bf282d2921fa20cecba779959ecf507808d7
Chris Gala <cg@chargeholdings.co>
2025-02-02 16:39:05 -0800

    Refactor Rails and Sidekiq logging reassignment

--- web/config/initializers/sidekiq.rb
+++ web/config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1').presence, network_timeout: 5 }
-  Rails.logger = Sidekiq::Logging.logger
+  Rails.logger = Sidekiq.logger
 end
 
 Sidekiq.configure_client do |config|
